289b6c41d7d0ae6812e47a37f1823462b31b07c7
ShashiKumar03 <shashikumarkushwaha3@gmail.com>
2023-11-23 18:52:59 +0530

    change

--- test/testMap.js
+++ test/testMap.js
@@ -2,7 +2,7 @@ const map = require("../map");
 
 const items = [1, 2, 3, 4, 5, 5];
 
-const answer = map(items, function (item) {
+const answers = map(items, function (item) {
   return item * item;
 });
-console.log(answer);
+console.log(answers);
